595dbccc508c8ffaa4bf1b33e9e19ec0eb5296e7
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-21 19:23:43 -0400

    references

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 // import User model 
 const User = require('./User');
+//import post model
+const Post = require('./Post');
 
 // export an object with User model as property
-module.exports = { User };
+module.exports = { User, Post };
