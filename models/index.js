e265ef3543d0d34e9e56f8aea6004bd54412996d
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-21 23:54:50 -0400

    put/delete posts

--- models/index.js
+++ models/index.js
@@ -3,5 +3,14 @@ const User = require('./User');
 //import post model
 const Post = require('./Post');
 
+// create association
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 // export an object with User model as property
 module.exports = { User, Post };
