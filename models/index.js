a34d6436d34b65400016fb24b3c311df086a539c
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-06-03 22:24:49 -0400

    associations

--- models/index.js
+++ models/index.js
@@ -2,6 +2,10 @@
 const User = require('./User');
 //import post model
 const Post = require('./Post');
+// import Vote model
+const Vote = require('./Vote');
+//import Comment model
+const Comment = require('./Comment');
 
 // create association
 User.hasMany(Post, {
@@ -12,5 +16,49 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 // export an object with User model as property
-module.exports = { User, Post };
+module.exports = { User, Post, Vote, Comment };
